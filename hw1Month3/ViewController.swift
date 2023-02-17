ef56f15ece1ae5f9504ab83824dd98625f7581e4
Datkayim <dkeldibekova08@gmail.com>
2023-02-17 18:16:13 +0600

    update

--- hw1Month3/ViewController.swift
+++ hw1Month3/ViewController.swift
@@ -8,7 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    @IBAction func login(_ sender: Any) {
+        if username.text?.isEmpty ?? true{
+            username.layer.borderWidth = 2
+            username.layer.borderColor = UIColor.red.cgColor
+        }
+        if password.text?.isEmpty ?? true{
+            password.layer.borderWidth = 2
+            password.layer.borderColor = UIColor.red.cgColor
+    }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
