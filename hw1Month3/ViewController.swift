1778c0585a18bde20edf7dc74a5befdce36e654b
Datkayim <dkeldibekova08@gmail.com>
2023-02-21 11:16:08 +0600

    update

--- hw1Month3/ViewController.swift
+++ hw1Month3/ViewController.swift
@@ -15,11 +15,29 @@ class ViewController: UIViewController {
         if username.text?.isEmpty ?? true{
             username.layer.borderWidth = 2
             username.layer.borderColor = UIColor.red.cgColor
+        }else{
+            username.layer.borderWidth = 0
         }
         if password.text?.isEmpty ?? true{
             password.layer.borderWidth = 2
             password.layer.borderColor = UIColor.red.cgColor
+        }else{
+            password.layer.borderWidth = 0
+        }
     }
+    @IBAction func logIn(_ sender: Any) {
+        if username.text?.isEmpty ?? true{
+            username.layer.borderWidth = 2
+            username.layer.borderColor = UIColor.red.cgColor
+        }else{
+            username.layer.borderWidth = 0
+        }
+        if password.text?.isEmpty ?? true{
+            password.layer.borderWidth = 2
+            password.layer.borderColor = UIColor.red.cgColor
+        }else{
+            password.layer.borderWidth = 0
+        }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
